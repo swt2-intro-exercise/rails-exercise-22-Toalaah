f0bad7e84e2f1b67bc147b0b9c5730692d2c84e8
Samuel Kunst <samuel.kunst@gmail.com>
2022-10-28 10:52:17 +0200

    allow editing paper's authors

--- app/controllers/papers_controller.rb
+++ app/controllers/papers_controller.rb
@@ -46,13 +46,14 @@ class PapersController < ApplicationController
   end
 
   private
+
     # Use callbacks to share common setup or constraints between actions.
     def set_paper
       @paper = Paper.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
-    def paper_params
-      params.require(:paper).permit(:title, :venue, :year)
-    end
+  # Only allow a list of trusted parameters through.
+  def paper_params
+    params.require(:paper).permit(:title, :venue, :year, { :author_ids => [] })
+  end
 end
