96ee1cd27864b5def7f45b13f5eb57cd4f9559e4
Samuel Kunst <samuel.kunst@gmail.com>
2022-10-28 08:57:37 +0200

    add field validation for paper model

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -1,2 +1,4 @@
 class Paper < ApplicationRecord
+  validates :title, :venue, :year, presence: true
+  validates :year, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Paper, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'with valid arguments' do
+    it 'should correctly initialize' do
+      paper = Paper.new(title: 'Test', venue: 'Venue', year: 1219)
+      expect(paper.title).to eq('Test')
+      expect(paper.venue).to eq('Venue')
+      expect(paper.year).to eq(1219)
+    end
+  end
+
+  context 'with invalid arguments' do
+    it 'is invalid with an empty title' do
+      paper = Paper.new(venue: 'Venue', year: 1219)
+      expect(paper).not_to be_valid
+    end
+
+    it 'is invalid with a negative year' do
+      paper = Paper.new(title: 'Test', venue: 'Venue', year: -100)
+      expect(paper).not_to be_valid
+    end
+  end
 end
